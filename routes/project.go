8a6bb3a83f672d14f9198ee26621cf2352a1ad6e
14yapkc1 <14yapkc1@gmail.com>
2020-06-29 14:45:06 -0700

    added test/test api

--- routes/project.go
+++ routes/project.go
@@ -30,6 +30,9 @@ func (app *App) RegisterProjectRoutes() {
 	app.router.HandleFunc("/projects/{id}/stub", app.GetProjectStub).Methods("GET")
 	app.router.HandleFunc("/projects/{id}/members", app.GetProjMembers).Methods("GET")
 
+	// TEST
+	app.router.HandleFunc("/test/test", app.Test).Methods("GET")
+
 	// TODO APIs
 
 	app.router.HandleFunc("/projects/{id}/pages/{page_name}/auth/{user_token}", app.CreateProjPage).Methods("POST")
@@ -40,6 +43,12 @@ func (app *App) RegisterProjectRoutes() {
 }
 
 // Creator APIs
+
+func (app *App) Test(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(r)
+	return
+}
 
 func (app *App) CreateProject(w http.ResponseWriter, r *http.Request) {
 	var newProject model.Project
